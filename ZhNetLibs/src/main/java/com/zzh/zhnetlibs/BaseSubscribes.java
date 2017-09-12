33eabdfc5dcbd3367b8646cb3057a94ce090733e
墨宇 <zzh_hz@126.com>
2017-09-12 19:49:34 +0800

    初始化项目，网络访问

---
+++ ZhNetLibs/src/main/java/com/zzh/zhnetlibs/BaseSubscribes.java
@@ -0,0 +1,37 @@
+package com.zzh.zhnetlibs;
+
+import io.reactivex.Observer;
+import io.reactivex.annotations.NonNull;
+import io.reactivex.disposables.Disposable;
+
+/**
+ * Created by ZZH on 2017/9/7.
+ *
+ * @Date: 2017/9/7
+ * @Email: zzh_hz@126.com
+ * @QQ: 1299234582
+ * @Author: zzh
+ * @Description:
+ */
+public class BaseSubscribes<T> implements Observer<T> {
+
+    @Override
+    public void onSubscribe(@NonNull Disposable d) {
+
+    }
+
+    @Override
+    public void onNext(T t) {
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+
+    }
+
+    @Override
+    public void onComplete() {
+
+    }
+}
